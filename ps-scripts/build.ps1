60bd3abc133c7c2f955c982cdd7aa27300893db3
William Sosropartono <wsosropartono@esri.com>
2024-02-06 09:16:26 -0800

    Updated scripts

--- ps-scripts/build.ps1
+++ ps-scripts/build.ps1
@@ -1,15 +1,13 @@
 
-function Run-Appium{
-    echo "${param.mobile_app}"
-    echo "${env.PWD}"
-
-    appium
-    emulator -avd Pixel_4_API_30
-}
 function Run{
-    Run-Appium
+    start powershell {Appium}
+    start powershell {emulator -avd Pixel_4_API_30}
+    start-sleep -seconds 5
+    echo pwd
+    Set-Location ".\ArcGIS-Earth-Mobile"; python script.py
+    Get-Location | select -ExpandProperty Path
+    echo "finished testing"
 }
-
 
 Run
 
