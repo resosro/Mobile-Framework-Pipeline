2078df6e0ea0fe5df2ca111b7da2d461bfb1dcc8
William Sosropartono <wsosropartono@esri.com>
2024-02-06 09:22:27 -0800

    Updated build and test scripts

--- ps-scripts/build.ps1
+++ ps-scripts/build.ps1
@@ -3,10 +3,7 @@ function Run{
     start powershell {Appium}
     start powershell {emulator -avd Pixel_4_API_30}
     start-sleep -seconds 5
-    echo pwd
-    Set-Location ".\ArcGIS-Earth-Mobile"; python script.py
-    Get-Location | select -ExpandProperty Path
-    echo "finished testing"
+    echo Get-Location | select -ExpandProperty Path
 }
 
 Run
--- ps-scripts/test.ps1
+++ ps-scripts/test.ps1
@@ -1,3 +1,11 @@
 # This will move all items from this dependencies folder over to the actual script
 # Does this have access to the envs?
-$Script = 
+
+function Run{
+    Set-Location ".\ArcGIS-Earth-Mobile"; python script.py
+}
+
+
+
+
+Run
